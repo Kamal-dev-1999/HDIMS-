3f92cb7e92a51ca6e69e0af38179988ff77b610b
Kamal-dev-1999 <137027258+Kamal-dev-1999@users.noreply.github.com>
2024-12-07 12:39:22 +0530

    Update views.py

--- patient_mobile/views.py
+++ patient_mobile/views.py
@@ -1,10 +1,13 @@
-from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from .serializers import PatientCreateUserSerializer, PatientLoginSerializer, PatientUserSerializer
-from rest_framework_simplejwt.tokens import RefreshToken
 
 class PatientSignupAPIView(APIView):
+    permission_classes = [AllowAny]  # Allow any user to access signup
+
     def post(self, request):
         serializer = PatientCreateUserSerializer(data=request.data)
         if serializer.is_valid():
@@ -12,7 +15,10 @@ class PatientSignupAPIView(APIView):
             return Response({"message": "Patient registered successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class PatientLoginAPIView(APIView):
+    permission_classes = [AllowAny]  # Allow any user to access login
+
     def post(self, request):
         serializer = PatientLoginSerializer(data=request.data)
         if serializer.is_valid():
